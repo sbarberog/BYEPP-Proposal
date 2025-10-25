2a1386f2d1f712c2cdd611a6070babbced405de6
sbarberog <sbarberog.90@gmail.com>
2025-10-25 23:04:16 +0200

    Fix responsive scaling 2

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function scrollRotate() {
     percent = window.pageYOffset / (pageHeight - windowHeight);
     pokeball.forEach(element => {
         element.style.transform = "rotate(" + percent * 360 + "deg)";
-        if(percent==1){
+        if (percent == 1) {
             element.classList.add('pokeball-end');
         }
-        else{
+        else {
             element.classList.remove('pokeball-end');
         }
     });
@@ -50,6 +50,13 @@ function handleMediaChange() {
                 transition: box-shadow 0.3s ease;}
             .poke-bottom:hover {
                 box-shadow: 0px 0px 40px rgba(249, 251, 255, 0.637);
+            }
+            .section:hover {
+                z-index: 1;
+                scale: 1.2;
+                transition-timing-function: cubic-bezier(0.34, 1.56, 0.64, 1);
+                box-shadow: 4px 4px 0 #ffcb05, 1px 1px 2px rgba(3, 7, 18, 0.116),
+                    3px 3px 8px rgba(3, 7, 18, 0.171), 6px 6px 19px rgba(3, 7, 18, 0.514);
             }
             `;
         console.log("Style: desktop");
